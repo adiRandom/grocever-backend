1ff856e49fe780b78aae8899cbf2be43aae73b2e
adi_random <me.adi.hf@gmail.com>
2022-12-01 13:45:47 +0200

    Added api calls for store service

--- ocr/api/store.go
+++ ocr/api/store.go
@@ -1,13 +1,43 @@
 package api
 
-import "lib/data/dto"
+import (
+	"lib/data/dto"
+	"lib/functional"
+	"lib/network/http"
+	"os"
+)
 
-func GetStoreMetadataForName(name string) (dto.StoreMetadata, error) {
-	// TODO: implement
-	return dto.StoreMetadata{Name: "MEGA IMAGE", OcrHeaderLines: 4}, nil
+type Store struct {
+	baseUrl string
 }
 
-func GetAllStoreNames() []string {
-	// TODO: implement
-	return []string{"AUCHAN", "CARREFOUR", "KAUFLAND", "MEGA IMAGE"}
+var client *Store
+
+func GetClient() *Store {
+	if client == nil {
+		client = &Store{
+			baseUrl: os.Getenv("STORES_API_BASE_URL"),
+		}
+	}
+	return client
+}
+
+func (s *Store) GetStoreMetadataForName(name string) (dto.StoreMetadata, error) {
+	res, err := http.GetSync[dto.StoreMetadata](s.baseUrl + "/store/" + name)
+	if err != nil {
+		return dto.StoreMetadata{}, err
+	}
+
+	return *res, nil
+}
+
+func (s *Store) GetAllStoreNames() []string {
+	res, err := http.GetSync[[]dto.StoreMetadata](s.baseUrl + "/store/list")
+	if err != nil {
+		return nil
+	}
+
+	return functional.Map[dto.StoreMetadata, string](*res, func(store dto.StoreMetadata) string {
+		return store.Name
+	})
 }
